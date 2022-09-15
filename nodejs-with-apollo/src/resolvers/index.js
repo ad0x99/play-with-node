7f093bf9c6f1fb4a6c4f55dfffb9f124bb9a945b
ad0x99 <anhduc.dev99@gmail.com>
2022-09-15 21:42:11 +0700

    feat: delete user

--- nodejs-with-apollo/src/resolvers/index.js
+++ nodejs-with-apollo/src/resolvers/index.js
@@ -5,11 +5,9 @@ import { v4 as uuidv4 } from 'uuid';
 const resolvers = {
   Query: {
     users: (parent, args, ctx, info) => {
-      if (args.data.name) {
+      if (args.name) {
         return users.filter((user) =>
-          formatSearchString(user.name).includes(
-            formatSearchString(args.data.name)
-          )
+          formatSearchString(user.name).includes(formatSearchString(args.name))
         );
       }
 
@@ -28,13 +26,11 @@ const resolvers = {
       if (args.title) {
         const isTitleMatch = posts.filter((post) =>
           formatSearchString(post.title).includes(
-            formatSearchString(args.data.title)
+            formatSearchString(args.title)
           )
         );
         const isBodyMatch = posts.filter((post) =>
-          formatSearchString(post.body).includes(
-            formatSearchString(args.data.title)
-          )
+          formatSearchString(post.body).includes(formatSearchString(args.title))
         );
 
         return isTitleMatch.length ? isTitleMatch : isBodyMatch;
@@ -67,6 +63,30 @@ const resolvers = {
       users.push(newUser);
 
       return newUser;
+    },
+
+    deleteUser(parent, args, ctx, info) {
+      const userIndex = users.findIndex((user) => user.id === args.id);
+      const postIndex = posts.findIndex((post) => post.author === args.id);
+      const commentIndex = comments.findIndex(
+        (comment) => comment.author === args.id
+      );
+
+      if (userIndex === -1) {
+        throw new Error('User not found');
+      }
+
+      if (postIndex !== -1) {
+        posts.splice(postIndex, 1);
+      }
+
+      if (commentIndex !== -1) {
+        comments.splice(commentIndex, 1);
+      }
+
+      const deletedUser = users.splice(userIndex, 1);
+
+      return deletedUser[0];
     },
 
     createPost(parent, args, ctx, info) {

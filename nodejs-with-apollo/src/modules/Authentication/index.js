a838717204cfd6f28dd08feb5bb32665856c56c2
ad0x99 <anhduc.dev99@gmail.com>
2022-09-20 22:10:39 +0700

    feat: handle authentication and authorization

---
+++ nodejs-with-apollo/src/modules/Authentication/index.js
@@ -0,0 +1,14 @@
+const generateJWTToken = () => {};
+const hashPassword = (password) => {};
+
+const isAuthenticated = (req, res, next) => {};
+const isAuthor = (req, res, next) => {};
+const isHasPermission = (req, res, next) => {};
+
+export {
+  isAuthenticated,
+  isAuthor,
+  isHasPermission,
+  generateJWTToken,
+  hashPassword,
+};
